13050be5ea65e8bd273bd1ee3b398b472c6a85bd
Todd Wolfson <todd@twolfson.com>
2014-12-19 19:03:21 -0600

    Fixing up commented code

--- test.js
+++ test.js
@@ -278,7 +278,8 @@ describe('interlace', function () {
     })
 
     it("should correctly read another indexed image", function(done) {
-      // return png.parseFile(path.join(__dirname, "data/s03i3p01.png"), function(err, id) {
+      // TODO: Put back 5x5 assertion
+      // return png.parseFile(path.join(__dirname, "data/s05i3p02.png"), function(err, id) {
       return png.parseFile(path.join(__dirname, "data/s08i3p02.png"), function(err, id) {
         assert.isUndefined(err)
         assert.equal(id.width, 3);
